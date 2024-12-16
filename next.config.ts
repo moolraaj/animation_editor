9e6a79f74de0b222d86739f6f4f3d2d504cf2727
neosharma995 <neosharma995@gmail.com>
2024-12-16 12:15:09 +0530

    Configure Next.js app for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'animation_editor';
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  basePath: isProd ? `/${repoName}` : '',
 };
 
 export default nextConfig;
