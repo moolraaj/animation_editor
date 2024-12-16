0c4341d4bddda843a1248fe00a7b6262b4933bb2
neosharma995 <neosharma995@gmail.com>
2024-12-16 12:25:35 +0530

    updated code

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'animation_editor';
 const nextConfig: NextConfig = {
-  output: 'export',
-  assetPrefix: isProd ? `/${repoName}/` : '',
-  basePath: isProd ? `/${repoName}` : '',
+  basePath: "/animation_editor",
+  output: "export",  
+  reactStrictMode: true,
 };
 
 export default nextConfig;
