3acc5273ddb0185cf1690e49d34b780a742bc6b3
neosharma995 <neosharma995@gmail.com>
2024-12-16 12:26:56 +0530

    updated code

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/animation_editor",
-  output: "export",  
-  reactStrictMode: true,
+  /* config options here */
 };
 
 export default nextConfig;
